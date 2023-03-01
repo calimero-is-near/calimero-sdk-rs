b39d542bcbbff80a3dc6344b876b3ad2510d8c61
Igor Jerkovic <igor@calimero.network>
2023-03-01 14:53:08 +0100

    Make cross shard call proc_macro_attribute work with near_bindgen (#1)
    Test plan:
    Deployed tictactoe example on NEAR and Calimero and verified cross shard calls work using the defined proc_macro_attribute
    
    Registered two players on NEAR contract:
    https://explorer.testnet.near.org/transactions/GrcKrEd6k8hz8s3cT2eqi2qxQf4sBb5fFys92Ehc3mmR
    and
    https://explorer.testnet.near.org/transactions/CD5YoU2MRmy3bRYJBYeXLixHPe2LipEYMat22UN9Uu2X
    The 2nd register triggered a CALIMERO_EVENT_CROSS_CALL
    
    From there the cross_call_execute happened on the Calimero private shard, and once the game started on Calimero, the callback got received on NEAR side again:
    https://explorer.testnet.near.org/transactions/F7EGvDcRHA91DkAYcFV9fJY8NH5MhyzTxgwB7bGWU1Fr

--- calimero-sdk-macros/src/lib.rs
+++ calimero-sdk-macros/src/lib.rs
@@ -1,175 +1,139 @@
 use proc_macro::TokenStream;
-use quote::{quote};
-use syn::{parse_macro_input, ItemFn, LitStr};
-use syn::{FnArg, Pat, PatType};
+use quote::quote;
+use syn::{LitStr, parse_macro_input, FnArg, parse_quote};
 
+const CALIMERO_RECEIVE_RESPONSE_ATTRIBUTE: &str = "calimero_receive_response";
+
+/// Modifies fn signature under impl for any fn denoted with #[calimero_receive_response]
+/// so that the last input arg of such function is renamed to `response` with Option<Vec<u8>> as
+/// type, the argument initially provided by the developer is used in generated code and into
+/// it - the response is deserialized. If the developer used optional type, error can be handled
+/// in the contract method, otherwise the method will panic if `response` is None
 #[proc_macro_attribute]
-pub fn flip_arguments(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let fn_type = parse_macro_input!(item as ItemFn);
+pub fn calimero_expand(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let ast: syn::ItemImpl = syn::parse(item).unwrap();
 
-    let _types: Vec<syn::Type> = fn_type
-        .sig.clone()
-        .inputs
-        .into_iter()
-        .filter_map(|arg| match arg {
-            syn::FnArg::Receiver(_) => None,
-            syn::FnArg::Typed(syn::PatType { ty, .. }) => Some(*ty),
-        })
-        .collect();
+    let mut methods = Vec::new();
+    for item in ast.items.clone() {
+        if let syn::ImplItem::Method(mut method) = item.clone() {
+            let has_special_attr = method.attrs.iter().any(|attr| attr.path.is_ident(CALIMERO_RECEIVE_RESPONSE_ATTRIBUTE));
 
-    let signature = fn_type.sig;
-    let ident = signature.ident;
-    let arguments = signature.inputs;
-    let return_type = signature.output;
+            if has_special_attr {
+                method.attrs.retain(|attr| !attr.path.is_ident(CALIMERO_RECEIVE_RESPONSE_ATTRIBUTE));
 
-    let block = fn_type.block;
+                if method.sig.inputs.len() != 2 {
+                    panic!("Method using the proc_macro_attribute #[calimero_receive_response] needs to have exactly two input arguments")
+                }
 
-    let attrs = fn_type.attrs;
-    let vis = fn_type.vis;
+                let args: Vec<syn::Pat> = method
+                    .sig.clone()
+                    .inputs
+                    .into_iter()
+                    .filter_map(|arg| match arg {
+                        syn::FnArg::Receiver(_) => None,
+                        syn::FnArg::Typed(syn::PatType { pat, .. }) => Some(*pat),
+                    })
+                    .collect();
 
-    let first_arg = arguments.first();
-    let second_arg = arguments.last();
+                let last_arg_ident = if let syn::Pat::Ident(x) = args.last().unwrap() {
+                    x
+                } else {
+                    panic!("Could not get ident.")
+                };
 
-    TokenStream::from(quote! {
-        #(#attrs)* #vis fn #ident ( #second_arg , #first_arg ) #return_type {
-            #block
+                let types: Vec<syn::Type> = method
+                    .sig.clone()
+                    .inputs
+                    .into_iter()
+                    .filter_map(|arg| match arg {
+                        syn::FnArg::Receiver(_) => None,
+                        syn::FnArg::Typed(syn::PatType { ty, .. }) => Some(*ty),
+                    })
+                    .collect();
+
+                let last_type_arg = types.last();
+                let input_arg_is_optional = is_optional(method.sig.inputs.last().unwrap());
+
+                let first_argument = method.sig.inputs.first().unwrap();
+                method.sig.inputs = parse_quote! {
+                    #first_argument, response: Option<Vec<u8>>
+                };
+
+                let old_block = method.block.clone();
+
+                let expanded =
+                    if input_arg_is_optional {
+                        quote! {
+                            {
+                                require!(env::predecessor_account_id().to_string() == CROSS_SHARD_CALL_CONTRACT_ID);
+                                let #last_arg_ident: #last_type_arg =
+                                if response.is_none() {
+                                    None
+                                } else {
+                                    near_sdk::serde_json::from_slice::<#last_type_arg>(&response.unwrap()).unwrap()
+                                };
+                                #old_block
+                            }
+                        }
+                    } else {
+                        quote! {
+                            {
+                                require!(env::predecessor_account_id().to_string() == CROSS_SHARD_CALL_CONTRACT_ID);
+                                if response.is_none() {
+                                    panic!("Expected cross shard call response, but received None");
+                                }
+                                let #last_arg_ident: #last_type_arg =
+                                    near_sdk::serde_json::from_slice::<#last_type_arg>(&response.unwrap()).unwrap();
+                                #old_block
+                            }
+                        }
+                    };
+
+                method.block = syn::parse2(expanded).unwrap();
+                methods.push(syn::ImplItem::Method(method));
+            } else {
+                methods.push(item);
+            }
+        } else {
+            methods.push(item);
         }
-    })
+    }
+
+    let struct_attrs = &ast.attrs;
+    let struct_name = &ast.self_ty;
+    let gen = quote! {
+        #(#struct_attrs)*
+        impl #struct_name {
+            #(#methods)*
+        }
+    };
+    gen.into()
 }
 
+fn is_optional(arg: &FnArg) -> bool {
+    if let FnArg::Typed(typed) = arg {
+        if let syn::Type::Path(path) = &*typed.ty {
+            if let Some(segment) = path.path.segments.last() {
+                if segment.ident.to_string() == "Option" {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
+
+/// This macro needs to be defined before making a cross_call, or receiving
+/// a cross_call_receive_response
+///
+/// E.g. before the definition of the contract
+/// calimero_cross_shard_connector!("xsc_connector.shard_id.dev.calimero.testnet");
 #[proc_macro]
-pub fn use_calimero(input: TokenStream) -> TokenStream {
+pub fn calimero_cross_shard_connector(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as LitStr);
 
     let generated_code = quote! {
         const CROSS_SHARD_CALL_CONTRACT_ID: &str = #input;
     };
     generated_code.into()
-}
-
-#[proc_macro_attribute]
-pub fn calimero_response_test(_attr: TokenStream, stream: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(stream as ItemFn);
-
-    let ItemFn { attrs, vis, sig, block } = input;
-    let stmts = &block.stmts;
-    let just_for_testing = quote! {
-        #(#attrs)* #vis #sig {
-            require!(env::predecessor_account_id().to_string() == CROSS_SHARD_CALL_CONTRACT_ID);
-            #(#stmts)*
-        }
-    };
-    just_for_testing.into()
-}
-
-// modifies fn signature so that the input arg is named response with Option<Vec<u8>> type
-// the initial argument is used in generated code and into it the response is deserialized
-#[proc_macro_attribute]
-pub fn calimero_receive_response1(_: TokenStream, item: TokenStream) -> TokenStream {
-    let fn_type = parse_macro_input!(item as ItemFn);
-
-    let types: Vec<syn::Type> = fn_type
-        .sig.clone()
-        .inputs
-        .into_iter()
-        .filter_map(|arg| match arg {
-            syn::FnArg::Receiver(_) => None,
-            syn::FnArg::Typed(syn::PatType { ty, .. }) => Some(*ty),
-        })
-        .collect();
-
-    let signature = fn_type.sig;
-    let ident = signature.ident;
-    let arguments = signature.inputs;
-    let args2 = arguments.clone();
-    let return_type = signature.output;
-
-    let _args = arguments.iter().map(|fn_arg| match fn_arg {
-        FnArg::Typed(PatType { pat, .. }) => match &**pat {
-            Pat::Ident(ident) => ident,
-            _ => panic!("argument pattern is not a simple ident"),
-        }
-        FnArg::Receiver(_) => panic!("argument is a receiver"),
-    });
-
-    let wanted_type = types.first().unwrap();
-    let block = fn_type.block;
-
-    TokenStream::from(quote! {
-        fn #ident ( response: Option<Vec<u8>> ) #return_type {
-            require!(env::predecessor_account_id().to_string() == CROSS_SHARD_CALL_CONTRACT_ID);
-            let #args2 =
-            if response.is_none() {
-                None
-            } else {
-                near_sdk::serde_json::from_slice::<#wanted_type>(&response.unwrap()).unwrap()
-            };
-            #block
-        }
-    })
-}
-
-// Similar as calimero_receive_response1 but leaving example here to see how to extract args and types
-// from function signature
-#[proc_macro_attribute]
-pub fn calimero_receive_response(_: TokenStream, item: TokenStream) -> TokenStream {
-    let fn_type = parse_macro_input!(item as ItemFn);
-    let attrs = fn_type.attrs;
-    let vis = fn_type.vis;
-    let signature = fn_type.sig.clone();
-    let ident = signature.ident;
-    let arguments = signature.inputs;
-    let return_type = signature.output;
-
-    let types: Vec<syn::Type> = fn_type
-        .sig.clone()
-        .inputs
-        .into_iter()
-        .filter_map(|arg| match arg {
-            syn::FnArg::Receiver(_) => None,
-            syn::FnArg::Typed(syn::PatType { ty, .. }) => Some(*ty),
-        })
-        .collect();
-
-    let wanted_arg = arguments.last().unwrap();
-
-    let last_type = types.last();
-
-    let block = fn_type.block;
-
-    if arguments.len() != 2 {
-        panic!("Method using the proc_macro_attribute needs to have two input arguments")
-    }
-
-    let first_arg = arguments.first();
-
-    let args: Vec<syn::Pat> = fn_type
-        .sig.clone()
-        .inputs
-        .into_iter()
-        .filter_map(|arg| match arg {
-            syn::FnArg::Receiver(_) => None,
-            syn::FnArg::Typed(syn::PatType { pat, .. }) => Some(*pat),
-        })
-        .collect();
-
-    let last_arg_ident = if let syn::Pat::Ident(x) = args.last().unwrap() {
-        x
-    } else {
-        panic!("Could not get ident.")
-    };
-
-    TokenStream::from(quote! {
-        #(#attrs)* #vis fn #ident ( #first_arg, #last_arg_ident: Option<Vec<u8>> ) #return_type {
-            require!(env::predecessor_account_id().to_string() == CROSS_SHARD_CALL_CONTRACT_ID);
-            let #wanted_arg =
-            if #last_arg_ident.is_none() {
-                None
-            } else {
-                near_sdk::serde_json::from_slice::<#last_type>(&#last_arg_ident.unwrap()).unwrap()
-            };
-
-            #block
-        }
-    })
 }
